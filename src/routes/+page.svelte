aa76dd568c1e36a4aedfbade00bfb0ca9667a2f0
M Imam Pratama <imambungo@gmail.com>
2023-08-07 08:46:21 +0700

    s

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -199,151 +199,122 @@
 		}, config) // https://plotly.com/javascript/line-charts/
 
 
-		const react = {
-			name: 'React',
-			mode: 'lines+markers',
-			text: ['2017', '2018', '2019', '2020', '2021', '2022', '2023'],
-			x: [     19.5,   27.8,   31.3,   35.9,  40.14,  42.62,  40.58], // popularity
-			y: [     66.9,   69.4,   74.5,   68.9,  69.28,  68.19,  63.61], // love
-			type: 'scatter'
-		}
+		const framework_data = [
+			{
+				name: 'React',
+				mode: 'lines+markers',
+				text: ['2017', '2018', '2019', '2020', '2021', '2022', '2023'],
+				x: [     19.5,   27.8,   31.3,   35.9,  40.14,  42.62,  40.58], // popularity
+				y: [     66.9,   69.4,   74.5,   68.9,  69.28,  68.19,  63.61], // love
+				type: 'scatter'
+			}, {
+				name: 'Angular',
+				mode: 'lines+markers',
+				text: ['2017', '2018', '2019', '2020', '2021', '2022', '2023'],
+				x: [     44.3,   36.9,   30.7,   25.1,  22.96,  20.39,  17.46], // popularity
+				y: [     51.7,   54.6,   57.6,   54.0,  55.82,  52.27,  50.75], // love
+				type: 'scatter'
+			}, {
+				name: 'jQuery',
+				mode: 'lines+markers',
+				text: ['2019', '2020', '2021', '2022', '2023'],
+				x: [     48.7,   43.3,  34.42,  28.57,  21.98], // popularity
+				y: [     45.3,   36.5,  35.29,   34.0,  32.89], // love
+				type: 'scatter'
+			}, {
+				name: 'Laravel',
+				mode: 'lines+markers',
+				text: ['2019', '2020', '2021', '2022', '2023'],
+				x: [     10.5,   11.1,  10.12,   9.45,   7.58], // popularity
+				y: [     60.1,   51.4,  53.79,  54.47,  52.63], // love
+				type: 'scatter'
+			}, {
+				name: 'Express',
+				mode: 'lines+markers',
+				text: ['2019', '2020', '2021', '2022', '2023'],
+				x: [     19.7,   21.2,  23.82,  22.99,  19.28], // popularity
+				y: [     68.3,   61.9,  62.07,  59.15,  54.26], // love
+				type: 'scatter'
+			}, {
+				name: 'Ruby on Rails',
+				mode: 'lines+markers',
+				text: ['2019', '2020', '2021', '2022', '2023'],
+				x: [      8.2,    7.0,   7.04,   5.83,   5.49], // popularity
+				y: [     57.1,   49.3,  57.55,  55.34,  52.97], // love
+				type: 'scatter'
+			}, {
+				name: 'Gatsby',
+				mode: 'lines+markers',
+				text: ['2020', '2021', '2022', '2023'],
+				x: [      4.0,   3.97,   3.46,   2.33], // popularity
+				y: [     60.7,  48.66,  35.57,  28.66], // love
+				type: 'scatter'
+			}, {
+				name: 'Svelte',
+				mode: 'lines+markers',
+				text: ['2021', '2022', '2023'],
+				x: [     2.75,   4.58,   6.62], // popularity
+				y: [    71.47,  75.28,  74.50], // love
+				type: 'scatter'
+			}, {
+				name: 'FastAPI',
+				mode: 'lines+markers',
+				text: ['2021', '2022', '2023'],
+				x: [     3.88,   6.02,   7.42], // popularity
+				y: [    70.04,  67.74,  64.49], // love
+				type: 'scatter'
+			}, {
+				name: 'Next.js',
+				mode: 'lines+markers',
+				text: ['2022', '2023'],
+				x: [    13.52,  16.67], // popularity
+				y: [    69.23,  65.95], // love
+				type: 'scatter'
+			}, {
+				name: 'Vue.js',
+				mode: 'lines+markers',
+				text: ['2019', '2020', '2021', '2022', '2023'],
+				x: [     15.2,   17.3,  18.97,  18.82,  16.38], // popularity
+				y: [     73.6,   66.0,  64.41,  63.16,  57.87], // love
+				type: 'scatter'
+			}, {
+				name: 'Node.js',
+				mode: 'lines+markers',
+				text: ['2017', '2018', '2019', '2020', '2021', '2022', '2023'],
+				x: [     47.1,   49.6,   49.9,   51.4,  33.91,  47.12,  42.65], // popularity
+				y: [     62.1,   66.4,   72.1,   66.8,  63.22,  67.36,  64.63], // love
+				type: 'scatter'
+			}, {
+				name: 'Django',
+				mode: 'lines+markers',
+				text: ['2018', '2019', '2020', '2021', '2022', '2023'],
+				x: [     13.0,   13.0,   14.2,  14.99,  14.65,  11.47], // popularity
+				y: [     58.3,   62.1,   55.3,  55.28,  53.10,  50.51], // love
+				type: 'scatter'
+			}, {
+				name: 'Flask',
+				mode: 'lines+markers',
+				text: ['2019', '2020', '2021', '2022', '2023'],
+				x: [     12.1,   14.2,  16.14,  14.64,  12.16], // popularity
+				y: [     61.1,   54.4,  51.04,  48.41,  45.36], // love
+				type: 'scatter'
+			}, {
+				name: 'WordPress',
+				mode: 'lines+markers',
+				text: ['2017', '2018', '2019', '2020', '2023'],
+				x: [     15.6,   15.9,   14.5,   14.2,  13.38], // popularity
+				y: [     35.5,   36.8,   40.5,   33.0,  32.52], // love
+				type: 'scatter'
+			}, {
+				name: 'Deno',
+				mode: 'lines+markers',
+				text: ['2021', '2022', '2023'],
+				x: [     1.41,   1.69,   2.36], // popularity
+				y: [    68.45,  72.32,  63.82], // love
+				type: 'scatter'
+			}
+		]
 
-		const angular = {
-			name: 'Angular',
-			mode: 'lines+markers',
-			text: ['2017', '2018', '2019', '2020', '2021', '2022', '2023'],
-			x: [     44.3,   36.9,   30.7,   25.1,  22.96,  20.39,  17.46], // popularity
-			y: [     51.7,   54.6,   57.6,   54.0,  55.82,  52.27,  50.75], // love
-			type: 'scatter'
-		}
-
-		const jquery = {
-			name: 'jQuery',
-			mode: 'lines+markers',
-			text: ['2019', '2020', '2021', '2022', '2023'],
-			x: [     48.7,   43.3,  34.42,  28.57,  21.98], // popularity
-			y: [     45.3,   36.5,  35.29,   34.0,  32.89], // love
-			type: 'scatter'
-		}
-
-		const laravel = {
-			name: 'Laravel',
-			mode: 'lines+markers',
-			text: ['2019', '2020', '2021', '2022', '2023'],
-			x: [     10.5,   11.1,  10.12,   9.45,   7.58], // popularity
-			y: [     60.1,   51.4,  53.79,  54.47,  52.63], // love
-			type: 'scatter'
-		}
-
-		const express = {
-			name: 'Express',
-			mode: 'lines+markers',
-			text: ['2019', '2020', '2021', '2022', '2023'],
-			x: [     19.7,   21.2,  23.82,  22.99,  19.28], // popularity
-			y: [     68.3,   61.9,  62.07,  59.15,  54.26], // love
-			type: 'scatter'
-		}
-
-		const rails = {
-			name: 'Ruby on Rails',
-			mode: 'lines+markers',
-			text: ['2019', '2020', '2021', '2022', '2023'],
-			x: [      8.2,    7.0,   7.04,   5.83,   5.49], // popularity
-			y: [     57.1,   49.3,  57.55,  55.34,  52.97], // love
-			type: 'scatter'
-		}
-
-		const gatsby = {
-			name: 'Gatsby',
-			mode: 'lines+markers',
-			text: ['2020', '2021', '2022', '2023'],
-			x: [      4.0,   3.97,   3.46,   2.33], // popularity
-			y: [     60.7,  48.66,  35.57,  28.66], // love
-			type: 'scatter'
-		}
-
-		const svelte = {
-			name: 'Svelte',
-			mode: 'lines+markers',
-			text: ['2021', '2022', '2023'],
-			x: [     2.75,   4.58,   6.62], // popularity
-			y: [    71.47,  75.28,  74.50], // love
-			type: 'scatter'
-		}
-
-		const fastapi = {
-			name: 'FastAPI',
-			mode: 'lines+markers',
-			text: ['2021', '2022', '2023'],
-			x: [     3.88,   6.02,   7.42], // popularity
-			y: [    70.04,  67.74,  64.49], // love
-			type: 'scatter'
-		}
-
-		const nextjs = {
-			name: 'Next.js',
-			mode: 'lines+markers',
-			text: ['2022', '2023'],
-			x: [    13.52,  16.67], // popularity
-			y: [    69.23,  65.95], // love
-			type: 'scatter'
-		}
-
-		const vue = {
-			name: 'Vue.js',
-			mode: 'lines+markers',
-			text: ['2019', '2020', '2021', '2022', '2023'],
-			x: [     15.2,   17.3,  18.97,  18.82,  16.38], // popularity
-			y: [     73.6,   66.0,  64.41,  63.16,  57.87], // love
-			type: 'scatter'
-		}
-
-		const nodejs = {
-			name: 'Node.js',
-			mode: 'lines+markers',
-			text: ['2017', '2018', '2019', '2020', '2021', '2022', '2023'],
-			x: [     47.1,   49.6,   49.9,   51.4,  33.91,  47.12,  42.65], // popularity
-			y: [     62.1,   66.4,   72.1,   66.8,  63.22,  67.36,  64.63], // love
-			type: 'scatter'
-		}
-
-		const django = {
-			name: 'Django',
-			mode: 'lines+markers',
-			text: ['2018', '2019', '2020', '2021', '2022', '2023'],
-			x: [     13.0,   13.0,   14.2,  14.99,  14.65,  11.47], // popularity
-			y: [     58.3,   62.1,   55.3,  55.28,  53.10,  50.51], // love
-			type: 'scatter'
-		}
-
-		const flask = {
-			name: 'Flask',
-			mode: 'lines+markers',
-			text: ['2019', '2020', '2021', '2022', '2023'],
-			x: [     12.1,   14.2,  16.14,  14.64,  12.16], // popularity
-			y: [     61.1,   54.4,  51.04,  48.41,  45.36], // love
-			type: 'scatter'
-		}
-
-		const wordpress = {
-			name: 'WordPress',
-			mode: 'lines+markers',
-			text: ['2017', '2018', '2019', '2020', '2023'],
-			x: [     15.6,   15.9,   14.5,   14.2,  13.38], // popularity
-			y: [     35.5,   36.8,   40.5,   33.0,  32.52], // love
-			type: 'scatter'
-		}
-
-		const deno = {
-			name: 'Deno',
-			mode: 'lines+markers',
-			text: ['2021', '2022', '2023'],
-			x: [     1.41,   1.69,   2.36], // popularity
-			y: [    68.45,  72.32,  63.82], // love
-			type: 'scatter'
-		}
-
-		const framework_data = [react, angular, jquery, laravel, express, svelte, nextjs, vue, nodejs, django, flask, fastapi, deno, wordpress, gatsby, rails]
 		Plotly.newPlot('framework', framework_data, {
 			...layout,
 			title: 'Web frameworks and technologies',
