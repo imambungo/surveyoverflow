6e7fc3ddcfd1c48f90320bd86afcd8b07e642c12
M Imam Pratama <imambungo@gmail.com>
2023-08-07 10:01:11 +0700

    s

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>Double-click anywhere on the plot area to reset the zoom level.</p>
 <div id="programming_languages" style="width:800px;height:800px;"></div> <!-- https://plotly.com/javascript/getting-started/ -->
 <div id="db" style="width:800px;height:800px;"></div>
 <div id="framework" style="width:800px;height:800px;"></div>
