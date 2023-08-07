04c31cafc4808ad0ce98517c7a3cf8830d456976
M Imam Pratama <imambungo@gmail.com>
2023-08-07 08:47:06 +0700

    s

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -322,25 +322,24 @@
 		}, config) // https://plotly.com/javascript/line-charts/
 
 
-		const react_native = {
-			name: 'React Native',
-			mode: 'lines+markers',
-			text: ['2019', '2020', '2021', '2022', '2023'],
-			x: [     10.5,   11.5,  14.51,  12.57,   8.43], // popularity
-			y: [     62.5,   57.9,  58.08,  55.98,   54.0], // love
-			type: 'scatter'
-		}
+		const other_data = [
+			{
+				name: 'React Native',
+				mode: 'lines+markers',
+				text: ['2019', '2020', '2021', '2022', '2023'],
+				x: [     10.5,   11.5,  14.51,  12.57,   8.43], // popularity
+				y: [     62.5,   57.9,  58.08,  55.98,   54.0], // love
+				type: 'scatter'
+			}, {
+				name: 'Flutter',
+				mode: 'lines+markers',
+				text: ['2019', '2020', '2021', '2022', '2023'],
+				x: [      3.4,    7.2,  13.55,  12.64,   9.12], // popularity
+				y: [     75.4,   68.8,  68.17,  68.03,  64.43], // love
+				type: 'scatter'
+			}
+		]
 
-		const flutter = {
-			name: 'Flutter',
-			mode: 'lines+markers',
-			text: ['2019', '2020', '2021', '2022', '2023'],
-			x: [      3.4,    7.2,  13.55,  12.64,   9.12], // popularity
-			y: [     75.4,   68.8,  68.17,  68.03,  64.43], // love
-			type: 'scatter'
-		}
-
-		const other_data = [react_native, flutter]
 		Plotly.newPlot('other', other_data, {
 			...layout,
 			title: 'Other frameworks and libraries',
