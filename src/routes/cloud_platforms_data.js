b3466350afe33adeb8528165ef35c76db2ea0af0
M Imam Pratama <imambungo@gmail.com>
2023-08-11 10:26:32 +0700

    s

---
+++ src/routes/cloud_platforms_data.js
@@ -0,0 +1,103 @@
+const cloud_platforms_data = [ // don't use data from before 2021 since they're not specifically cloud platforms
+   {
+      name: 'Firebase',
+      mode: 'lines+markers',
+      text: ['2022', '2023'],
+      x: [    21.14,  15.47], // popularity
+      y: [    57.45,  54.46], // love
+      type: 'scatter'
+   }, {
+      name: 'Linode, now Akamai',
+      mode: 'lines+markers',
+      text: ['2022', '2023'],
+      x: [     3.92,   3.96], // popularity
+      y: [    56.47,  49.98], // love
+      type: 'scatter'
+   }, {
+      name: 'Cloudflare',
+      mode: 'lines+markers',
+      text: ['2023'],
+      x: [    15.24], // popularity
+      y: [    66.19], // love
+      type: 'scatter'
+   }, {
+      name: 'Hetzner',
+      mode: 'lines+markers',
+      text: ['2023'],
+      x: [     4.41], // popularity
+      y: [    70.09], // love
+      type: 'scatter'
+   }, {
+      name: 'Vercel',
+      mode: 'lines+markers',
+      text: ['2023'],
+      x: [    10.68], // popularity
+      y: [    69.18], // love
+      type: 'scatter'
+   }, {
+      name: 'Vultr',
+      mode: 'lines+markers',
+      text: ['2023'],
+      x: [     1.95], // popularity
+      y: [    50.77], // love
+      type: 'scatter'
+   }, {
+      name: 'Netlify',
+      mode: 'lines+markers',
+      text: ['2023'],
+      x: [     8.95], // popularity
+      y: [    52.73], // love
+      type: 'scatter'
+   }, {
+      name: 'Google Cloud',
+      mode: 'lines+markers',
+      text: ['2021', '2022', '2023'],
+      x: [    31.05,  26.81,  23.86], // popularity
+      y: [    59.65,  58.18,  55.13], // love
+      type: 'scatter'
+   }, {
+      name: 'Microsoft Azure',
+      mode: 'lines+markers',
+      text: ['2021', '2022', '2023'],
+      x: [    30.77,  28.72,  26.03], // popularity
+      y: [    59.23,  59.42,  59.93], // love
+      type: 'scatter'
+   }, {
+      name: 'DigitalOcean',
+      mode: 'lines+markers',
+      text: ['2021', '2022', '2023'],
+      x: [    17.67,  15.64,  13.37], // popularity
+      y: [    58.85,  57.61,  53.04], // love
+      type: 'scatter'
+   }, {
+      name: 'Heroku',
+      mode: 'lines+markers',
+      text: ['2021', '2022', '2023'],
+      x: [       24,  19.98,  12.02], // popularity
+      y: [    48.17,  43.65,  21.97], // love
+      type: 'scatter'
+   }, {
+      name: 'Oracle Cloud Infrastructure (OCI)',
+      mode: 'lines+markers',
+      text: ['2021', '2022', '2023'],
+      x: [     1.89,   2.18,    2.6], // popularity
+      y: [    42.80,  50.90,  55.08], // love
+      type: 'scatter'
+   }, {
+      name: 'IBM Cloud or Watson',
+      mode: 'lines+markers',
+      text: ['2021', '2022', '2023'],
+      x: [     2.55,   1.68,   1.15], // popularity
+      y: [    37.29,  36.69,  32.21], // love
+      type: 'scatter'
+   }, {
+      name: 'AWS',
+      mode: 'lines+markers',
+      text: ['2021', '2022', '2023'],
+      x: [    54.22,  51.01,  48.62], // popularity
+      y: [    66.55,  66.32,  62.24], // love
+      type: 'scatter'
+   }
+]
+
+export default cloud_platforms_data
