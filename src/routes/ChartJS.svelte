27cf6288e3bc5c9fd14333603e40943fe8651ea2
M Imam Pratama <imambungo@gmail.com>
2023-08-18 12:21:35 +0700

    s

--- src/routes/ChartJS.svelte
+++ src/routes/ChartJS.svelte
@@ -10,11 +10,12 @@
 		ScatterController, LinearScale, PointElement, LineElement,
 		LineController,
 	} from 'chart.js' // https://www.chartjs.org/docs/latest/getting-started/usage.html#build-a-new-application-with-chart-js | https://stackoverflow.com/a/67143648/9157799
-
 	import zoomPlugin from "chartjs-plugin-zoom" // https://www.chartjs.org/docs/latest/#features | https://www.chartjs.org/chartjs-plugin-zoom/latest/guide/integration.html | https://stackoverflow.com/a/76910295/9157799
+   import annotationPlugin from 'chartjs-plugin-annotation' // https://www.chartjs.org/chartjs-plugin-annotation/latest/guide/
 
    Chart.register(
 		zoomPlugin,                                                // https://www.chartjs.org/chartjs-plugin-zoom/latest/guide/integration.html | https://www.chartjs.org/docs/latest/developers/plugins.html#global-plugins
+      annotationPlugin,                                          // https://www.chartjs.org/chartjs-plugin-annotation/latest/guide/integration.html
 		BarController, CategoryScale, BarElement,                  // https://stackoverflow.com/a/67143648/9157799
 		ScatterController, LinearScale, PointElement, LineElement,
 		LineController,
@@ -37,8 +38,8 @@
 						yAxisKey: 'love',
 					},
 					showLine: true, // https://www.chartjs.org/docs/latest/charts/scatter.html#dataset-properties
-					plugins: { // https://www.chartjs.org/chartjs-plugin-zoom/latest/guide/usage.html
-						zoom: {
+					plugins: { 
+						zoom: { // https://www.chartjs.org/chartjs-plugin-zoom/latest/guide/usage.html
 							zoom: {
 								wheel: {
 									enabled: true,
@@ -51,7 +52,21 @@
 							pan: {
 								enabled: true
 							},
-						}
+						},
+                  annotation: { // https://www.chartjs.org/chartjs-plugin-annotation/latest/guide/usage.html
+                     annotations: {
+                        label1: {
+                           type: 'label',
+                           xValue: 2.5,
+                           yValue: 60,
+                           backgroundColor: 'rgba(245,245,245)',
+                           content: ['This is my text', 'This is my text, second line'],
+                           font: {
+                              size: 18
+                           }
+                        }
+                     }
+                  }
 					},
 				},
 			}
