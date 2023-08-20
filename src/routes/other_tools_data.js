4c3a725779485b4b74730a017c01714e0f44c1eb
M Imam Pratama <imambungo@gmail.com>
2023-08-20 10:47:28 +0700

    s

--- src/routes/other_tools_data.js
+++ src/routes/other_tools_data.js
@@ -1,39 +1,68 @@
-const other_tools_data = [  // 2022 onwards. Don't use data from 2021 because git is removed since 2022.
+const other_tools_data = [ // 2022 onwards. Don't use data from 2021 because git is removed since 2022.
    {
-      name: 'Docker',
-      mode: 'lines+markers',
-      text: ['2022', '2023'],
-      x: [    63.72,  51.55], // popularity
-      y: [    76.93,  74.99], // love
-      type: 'scatter'
-   }, {
-      name: 'Yarn',
-      mode: 'lines+markers',
-      text: ['2022', '2023'],
-      x: [    27.64,  21.86], // popularity
-      y: [    61.90,  54.44], // love
-      type: 'scatter'
-   }, {
-      name: 'pnpm',
-      mode: 'lines+markers',
-      text: ['2023'],
-      x: [     6.28], // popularity
-      y: [    71.86], // love
-      type: 'scatter'
-   }, {
-      name: 'Bun',
-      mode: 'lines+markers',
-      text: ['2023'],
-      x: [     0.77], // popularity
-      y: [    66.77], // love
-      type: 'scatter'
-   }, {
-      name: 'npm',
-      mode: 'lines+markers',
-      text: ['2022', '2023'],
-      x: [    65.17,  49.36], // popularity
-      y: [    62.85,  59.67], // love
-      type: 'scatter'
+      label: 'Docker',
+      data: [
+         {
+            year: '2022',
+            popularity: 63.72,
+            love: 76.93
+         },
+         {
+            year: '2023',
+            popularity: 51.55,
+            love: 74.99
+         }
+      ]
+   },
+   {
+      label: 'Yarn',
+      data: [
+         {
+            year: '2022',
+            popularity: 27.64,
+            love: 61.90
+         },
+         {
+            year: '2023',
+            popularity: 21.86,
+            love: 54.44
+         }
+      ]
+   },
+   {
+      label: 'pnpm',
+      data: [
+         {
+            year: '2023',
+            popularity: 6.28,
+            love: 71.86
+         }
+      ]
+   },
+   {
+      label: 'Bun',
+      data: [
+         {
+            year: '2023',
+            popularity: 0.77,
+            love: 66.77
+         }
+      ]
+   },
+   {
+      label: 'npm',
+      data: [
+         {
+            year: '2022',
+            popularity: 65.17,
+            love: 62.85
+         },
+         {
+            year: '2023',
+            popularity: 49.36,
+            love: 59.67
+         }
+      ]
    }
 ]
 
