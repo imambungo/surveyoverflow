01323cec4c74bea9ac78e7754cbf776147301850
M Imam Pratama <imambungo@gmail.com>
2023-08-20 10:43:21 +0700

    s

--- src/routes/web_frameworks_data.js
+++ src/routes/web_frameworks_data.js
@@ -1,137 +1,533 @@
 const web_frameworks_data = [
    {
-      name: 'React',
-      mode: 'lines+markers',
-      text: ['2017', '2018', '2019', '2020', '2021', '2022', '2023'],
-      x: [     19.5,   27.8,   31.3,   35.9,  40.14,  42.62,  40.58], // popularity
-      y: [     66.9,   69.4,   74.5,   68.9,  69.28,  68.19,  63.61], // love
-      type: 'scatter'
-   }, {
-      name: 'Angular',
-      mode: 'lines+markers',
-      text: ['2017', '2018', '2019', '2020', '2021', '2022', '2023'],
-      x: [     44.3,   36.9,   30.7,   25.1,  22.96,  20.39,  17.46], // popularity
-      y: [     51.7,   54.6,   57.6,   54.0,  55.82,  52.27,  50.75], // love
-      type: 'scatter'
-   }, {
-      name: 'jQuery',
-      mode: 'lines+markers',
-      text: ['2019', '2020', '2021', '2022', '2023'],
-      x: [     48.7,   43.3,  34.42,  28.57,  21.98], // popularity
-      y: [     45.3,   36.5,  35.29,   34.0,  32.89], // love
-      type: 'scatter'
-   }, {
-      name: 'Laravel',
-      mode: 'lines+markers',
-      text: ['2019', '2020', '2021', '2022', '2023'],
-      x: [     10.5,   11.1,  10.12,   9.45,   7.58], // popularity
-      y: [     60.1,   51.4,  53.79,  54.47,  52.63], // love
-      type: 'scatter'
-   }, {
-      name: 'ASP.NET',
-      mode: 'lines+markers',
-      text: ['2019', '2020', '2021', '2022', '2023'],
-      x: [     26.3,   21.9,  15.74,   14.9,  12.79], // popularity
-      y: [     64.9,   36.9,  36.40,  37.63,  38.26], // love
-      type: 'scatter'
-   }, {
-      name: 'ASP.NET Core',
-      mode: 'lines+markers',
-      text: ['2020', '2021', '2022', '2023'],
-      x: [     19.1,   18.1,  18.59,  16.57], // popularity
-      y: [     70.7,  71.47,  71.49,  70.89], // love
-      type: 'scatter'
-   }, {
-      name: 'Spring Boot',
-      mode: 'lines+markers',
-      text: ['2023'],
-      x: [    11.95], // popularity
-      y: [    59.49], // love
-      type: 'scatter'
-   }, {
-      name: 'Express',
-      mode: 'lines+markers',
-      text: ['2019', '2020', '2021', '2022', '2023'],
-      x: [     19.7,   21.2,  23.82,  22.99,  19.28], // popularity
-      y: [     68.3,   61.9,  62.07,  59.15,  54.26], // love
-      type: 'scatter'
-   }, {
-      name: 'Ruby on Rails',
-      mode: 'lines+markers',
-      text: ['2019', '2020', '2021', '2022', '2023'],
-      x: [      8.2,    7.0,   7.04,   5.83,   5.49], // popularity
-      y: [     57.1,   49.3,  57.55,  55.34,  52.97], // love
-      type: 'scatter'
-   }, {
-      name: 'Gatsby',
-      mode: 'lines+markers',
-      text: ['2020', '2021', '2022', '2023'],
-      x: [      4.0,   3.97,   3.46,   2.33], // popularity
-      y: [     60.7,  48.66,  35.57,  28.66], // love
-      type: 'scatter'
-   }, {
-      name: 'Svelte',
-      mode: 'lines+markers',
-      text: ['2021', '2022', '2023'],
-      x: [     2.75,   4.58,   6.62], // popularity
-      y: [    71.47,  75.28,  74.50], // love
-      type: 'scatter'
-   }, {
-      name: 'FastAPI',
-      mode: 'lines+markers',
-      text: ['2021', '2022', '2023'],
-      x: [     3.88,   6.02,   7.42], // popularity
-      y: [    70.04,  67.74,  64.49], // love
-      type: 'scatter'
-   }, {
-      name: 'Next.js',
-      mode: 'lines+markers',
-      text: ['2022', '2023'],
-      x: [    13.52,  16.67], // popularity
-      y: [    69.23,  65.95], // love
-      type: 'scatter'
-   }, {
-      name: 'Vue.js',
-      mode: 'lines+markers',
-      text: ['2019', '2020', '2021', '2022', '2023'],
-      x: [     15.2,   17.3,  18.97,  18.82,  16.38], // popularity
-      y: [     73.6,   66.0,  64.41,  63.16,  57.87], // love
-      type: 'scatter'
-   }, {
-      name: 'Node.js',
-      mode: 'lines+markers',
-      text: ['2017', '2018', '2019', '2020', '2021', '2022', '2023'],
-      x: [     47.1,   49.6,   49.9,   51.4,  33.91,  47.12,  42.65], // popularity
-      y: [     62.1,   66.4,   72.1,   66.8,  63.22,  67.36,  64.63], // love
-      type: 'scatter'
-   }, {
-      name: 'Django',
-      mode: 'lines+markers',
-      text: ['2018', '2019', '2020', '2021', '2022', '2023'],
-      x: [     13.0,   13.0,   14.2,  14.99,  14.65,  11.47], // popularity
-      y: [     58.3,   62.1,   55.3,  55.28,  53.10,  50.51], // love
-      type: 'scatter'
-   }, {
-      name: 'Flask',
-      mode: 'lines+markers',
-      text: ['2019', '2020', '2021', '2022', '2023'],
-      x: [     12.1,   14.2,  16.14,  14.64,  12.16], // popularity
-      y: [     61.1,   54.4,  51.04,  48.41,  45.36], // love
-      type: 'scatter'
-   }, {
-      name: 'WordPress',
-      mode: 'lines+markers',
-      text: ['2017', '2018', '2019', '2020', '2023'],
-      x: [     15.6,   15.9,   14.5,   14.2,  13.38], // popularity
-      y: [     35.5,   36.8,   40.5,   33.0,  32.52], // love
-      type: 'scatter'
-   }, {
-      name: 'Deno',
-      mode: 'lines+markers',
-      text: ['2021', '2022', '2023'],
-      x: [     1.41,   1.69,   2.36], // popularity
-      y: [    68.45,  72.32,  63.82], // love
-      type: 'scatter'
+      label: 'React',
+      data: [
+         {
+            year: '2017',
+            popularity: 19.5,
+            love: 66.9
+         },
+         {
+            year: '2018',
+            popularity: 27.8,
+            love: 69.4
+         },
+         {
+            year: '2019',
+            popularity: 31.3,
+            love: 74.5
+         },
+         {
+            year: '2020',
+            popularity: 35.9,
+            love: 68.9
+         },
+         {
+            year: '2021',
+            popularity: 40.14,
+            love: 69.28
+         },
+         {
+            year: '2022',
+            popularity: 42.62,
+            love: 68.19
+         },
+         {
+            year: '2023',
+            popularity: 40.58,
+            love: 63.61
+         }
+      ]
+   },
+   {
+      label: 'Angular',
+      data: [
+         {
+            year: '2017',
+            popularity: 44.3,
+            love: 51.7
+         },
+         {
+            year: '2018',
+            popularity: 36.9,
+            love: 54.6
+         },
+         {
+            year: '2019',
+            popularity: 30.7,
+            love: 57.6
+         },
+         {
+            year: '2020',
+            popularity: 25.1,
+            love: 54.0
+         },
+         {
+            year: '2021',
+            popularity: 22.96,
+            love: 55.82
+         },
+         {
+            year: '2022',
+            popularity: 20.39,
+            love: 52.27
+         },
+         {
+            year: '2023',
+            popularity: 17.46,
+            love: 50.75
+         }
+      ]
+   },
+   {
+      label: 'jQuery',
+      data: [
+         {
+            year: '2019',
+            popularity: 48.7,
+            love: 45.3
+         },
+         {
+            year: '2020',
+            popularity: 43.3,
+            love: 36.5
+         },
+         {
+            year: '2021',
+            popularity: 34.42,
+            love: 35.29
+         },
+         {
+            year: '2022',
+            popularity: 28.57,
+            love: 34.0
+         },
+         {
+            year: '2023',
+            popularity: 21.98,
+            love: 32.89
+         }
+      ]
+   },
+   {
+      label: 'Laravel',
+      data: [
+         {
+            year: '2019',
+            popularity: 10.5,
+            love: 60.1
+         },
+         {
+            year: '2020',
+            popularity: 11.1,
+            love: 51.4
+         },
+         {
+            year: '2021',
+            popularity: 10.12,
+            love: 53.79
+         },
+         {
+            year: '2022',
+            popularity: 9.45,
+            love: 54.47
+         },
+         {
+            year: '2023',
+            popularity: 7.58,
+            love: 52.63
+         }
+      ]
+   },
+   {
+      label: 'ASP.NET',
+      data: [
+         {
+            year: '2019',
+            popularity: 26.3,
+            love: 64.9
+         },
+         {
+            year: '2020',
+            popularity: 21.9,
+            love: 36.9
+         },
+         {
+            year: '2021',
+            popularity: 15.74,
+            love: 36.40
+         },
+         {
+            year: '2022',
+            popularity: 14.9,
+            love: 37.63
+         },
+         {
+            year: '2023',
+            popularity: 12.79,
+            love: 38.26
+         }
+      ]
+   },
+   {
+      label: 'ASP.NET Core',
+      data: [
+         {
+            year: '2020',
+            popularity: 19.1,
+            love: 70.7
+         },
+         {
+            year: '2021',
+            popularity: 18.1,
+            love: 71.47
+         },
+         {
+            year: '2022',
+            popularity: 18.59,
+            love: 71.49
+         },
+         {
+            year: '2023',
+            popularity: 16.57,
+            love: 70.89
+         }
+      ]
+   },
+   {
+      label: 'Spring Boot',
+      data: [
+         {
+            year: '2023',
+            popularity: 11.95,
+            love: 59.49
+         }
+      ]
+   },
+   {
+      label: 'Express',
+      data: [
+         {
+            year: '2019',
+            popularity: 19.7,
+            love: 68.3
+         },
+         {
+            year: '2020',
+            popularity: 21.2,
+            love: 61.9
+         },
+         {
+            year: '2021',
+            popularity: 23.82,
+            love: 62.07
+         },
+         {
+            year: '2022',
+            popularity: 22.99,
+            love: 59.15
+         },
+         {
+            year: '2023',
+            popularity: 19.28,
+            love: 54.26
+         }
+      ]
+   },
+   {
+      label: 'Ruby on Rails',
+      data: [
+         {
+            year: '2019',
+            popularity: 8.2,
+            love: 57.1
+         },
+         {
+            year: '2020',
+            popularity: 7.0,
+            love: 49.3
+         },
+         {
+            year: '2021',
+            popularity: 7.04,
+            love: 57.55
+         },
+         {
+            year: '2022',
+            popularity: 5.83,
+            love: 55.34
+         },
+         {
+            year: '2023',
+            popularity: 5.49,
+            love: 52.97
+         }
+      ]
+   },
+   {
+      label: 'Gatsby',
+      data: [
+         {
+            year: '2020',
+            popularity: 4.0,
+            love: 60.7
+         },
+         {
+            year: '2021',
+            popularity: 3.97,
+            love: 48.66
+         },
+         {
+            year: '2022',
+            popularity: 3.46,
+            love: 35.57
+         },
+         {
+            year: '2023',
+            popularity: 2.33,
+            love: 28.66
+         }
+      ]
+   },
+   {
+      label: 'Svelte',
+      data: [
+         {
+            year: '2021',
+            popularity: 2.75,
+            love: 71.47
+         },
+         {
+            year: '2022',
+            popularity: 4.58,
+            love: 75.28
+         },
+         {
+            year: '2023',
+            popularity: 6.62,
+            love: 74.50
+         }
+      ]
+   },
+   {
+      label: 'FastAPI',
+      data: [
+         {
+            year: '2021',
+            popularity: 3.88,
+            love: 70.04
+         },
+         {
+            year: '2022',
+            popularity: 6.02,
+            love: 67.74
+         },
+         {
+            year: '2023',
+            popularity: 7.42,
+            love: 64.49
+         }
+      ]
+   },
+   {
+      label: 'Next.js',
+      data: [
+         {
+            year: '2022',
+            popularity: 13.52,
+            love: 69.23
+         },
+         {
+            year: '2023',
+            popularity: 16.67,
+            love: 65.95
+         }
+      ]
+   },
+   {
+      label: 'Vue.js',
+      data: [
+         {
+            year: '2019',
+            popularity: 15.2,
+            love: 73.6
+         },
+         {
+            year: '2020',
+            popularity: 17.3,
+            love: 66.0
+         },
+         {
+            year: '2021',
+            popularity: 18.97,
+            love: 64.41
+         },
+         {
+            year: '2022',
+            popularity: 18.82,
+            love: 63.16
+         },
+         {
+            year: '2023',
+            popularity: 16.38,
+            love: 57.87
+         }
+      ]
+   },
+   {
+      label: 'Node.js',
+      data: [
+         {
+            year: '2017',
+            popularity: 47.1,
+            love: 62.1
+         },
+         {
+            year: '2018',
+            popularity: 49.6,
+            love: 66.4
+         },
+         {
+            year: '2019',
+            popularity: 49.9,
+            love: 72.1
+         },
+         {
+            year: '2020',
+            popularity: 51.4,
+            love: 66.8
+         },
+         {
+            year: '2021',
+            popularity: 33.91,
+            love: 63.22
+         },
+         {
+            year: '2022',
+            popularity: 47.12,
+            love: 67.36
+         },
+         {
+            year: '2023',
+            popularity: 42.65,
+            love: 64.63
+         }
+      ]
+   },
+   {
+      label: 'Django',
+      data: [
+         {
+            year: '2018',
+            popularity: 13.0,
+            love: 58.3
+         },
+         {
+            year: '2019',
+            popularity: 13.0,
+            love: 62.1
+         },
+         {
+            year: '2020',
+            popularity: 14.2,
+            love: 55.3
+         },
+         {
+            year: '2021',
+            popularity: 14.99,
+            love: 55.28
+         },
+         {
+            year: '2022',
+            popularity: 14.65,
+            love: 53.10
+         },
+         {
+            year: '2023',
+            popularity: 11.47,
+            love: 50.51
+         }
+      ]
+   },
+   {
+      label: 'Flask',
+      data: [
+         {
+            year: '2019',
+            popularity: 12.1,
+            love: 61.1
+         },
+         {
+            year: '2020',
+            popularity: 14.2,
+            love: 54.4
+         },
+         {
+            year: '2021',
+            popularity: 16.14,
+            love: 51.04
+         },
+         {
+            year: '2022',
+            popularity: 14.64,
+            love: 48.41
+         },
+         {
+            year: '2023',
+            popularity: 12.16,
+            love: 45.36
+         }
+      ]
+   },
+   {
+      label: 'WordPress',
+      data: [
+         {
+            year: '2017',
+            popularity: 15.6,
+            love: 35.5
+         },
+         {
+            year: '2018',
+            popularity: 15.9,
+            love: 36.8
+         },
+         {
+            year: '2019',
+            popularity: 14.5,
+            love: 40.5
+         },
+         {
+            year: '2020',
+            popularity: 14.2,
+            love: 33.0
+         },
+         {
+            year: '2023',
+            popularity: 13.38,
+            love: 32.52
+         }
+      ]
+   },
+   {
+      label: 'Deno',
+      data: [
+         {
+            year: '2021',
+            popularity: 1.41,
+            love: 68.45
+         },
+         {
+            year: '2022',
+            popularity: 1.69,
+            love: 72.32
+         },
+         {
+            year: '2023',
+            popularity: 2.36,
+            love: 63.82
+         }
+      ]
    }
 ]
 
